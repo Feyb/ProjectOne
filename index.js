650b11c06e410f9173ff3a6730906bf84c6d6779
Fabio Caruso <caruso.fabio@hotmail.com>
2023-05-27 11:17:48 +0200

    updated project structure

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const app = express();
+const router = express.Router();
+
+const port = 3000;
+app.use(express.static('source/public'));
+
+/* POST create */
+router.post('/create', (req, res) => {
+  let { title } = req.body;
+  let { desc } = req.body;
+  let importance = req.body.range_value;
+  let date = req.body.toDoUntil;
+  // var date = dateorg.slice(0,10);
+  let createDate = new Date().toLocaleString();
+
+  let note = {
+    title,
+    description: desc,
+    importance,
+    date,
+    isFinished: "",
+    createdate: createDate
+  };
+
+  data.addNote(note);
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening at http://localhost:${port}`);
+});
